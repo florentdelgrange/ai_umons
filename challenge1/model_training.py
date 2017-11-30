d7f3614a3ac375dfced10863d3524cc1b37ac665
radioGiorgio <florent.delgrange@gmail.com>
2017-11-30 12:27:29 +0100

    challenge1

---
+++ challenge1/model_training.py
@@ -0,0 +1,52 @@
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.layers import Dense, GlobalAveragePooling2D
+
+base_model = InceptionResNetV2(include_top=False)
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+# and a logistic layer ; we have 2 classes
+predictions = Dense(2, activation='softmax')(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+# compile the model (should be done *after* setting layers to non-trainable)
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+# train the model on the new data for a few epochs
+# Example : model.fit_generator(generate_arrays_from_file('/my_file.txt'),
+#                    steps_per_epoch=1000, epochs=10)
+model.fit_generator(...)
+
+# at this point, the top layers are well trained and we can start fine-tuning
+# We will freeze the bottom N layers
+# and train the remaining top layers.
+
+# let's visualize layer names and layer indices to see how many layers
+# we should freeze:
+for i, layer in enumerate(base_model.layers):
+   print(i, layer.name)
+
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 249 layers and unfreeze the rest:
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+
+# we need to recompile the model for these modifications to take effect
+# we use SGD with a low learning rate
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+model.fit_generator(...)
