d1f2b5f29adfd03761322f78f5799b0b2b8ab968
radioGiorgio <florent.delgrange@gmail.com>
2017-12-18 13:17:20 +0100

    verbose changed

--- challenge1/validation.py
+++ challenge1/validation.py
@@ -1,3 +1,5 @@
+import progressbar
+# see https://github.com/coagulant/progressbar-python3
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
@@ -16,22 +18,34 @@ def preprocess_input(x):
     x *= 2.
     return x
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 if __name__ == '__main__':
 
     logs = Logger(filename='{}/accuracy.log'.format(DROPBOX_PATH))
 
     # load json and create model
-    json_file = open('model.json', 'r')
+    json_file = open('xception_gender.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
     # load weights into new model
-    model.load_weights("model.h5")
+    model.load_weights("xception_gender.h5")
 
     predictions = []
 
+    length = file_len('sorted_faces/valid/valid_info.txt')
+
+    bar = progressbar.ProgressBar(maxval=length, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+
     with open('sorted_faces/valid/valid_info.txt', 'r') as f:
-        for line in f:
+        for i, line in enumerate(f):
             name, gender, age = line.split(' ; ')
             img_path = 'sorted_faces/valid/all/{}'.format(name)
             img = image.load_img(img_path, target_size=(299, 299))
@@ -45,7 +59,9 @@ if __name__ == '__main__':
             # (one such list for each sample in the batch)
             p = preds[0][0]
             predictions.append((p<0.5 and gender=='m') or (p>=0.5 and gender=='f'))
-            print('{} : y={} | y_pred={} (F: {:.2f} %)'.format(len(predictions), gender, {True: 'm', False: 'f'}[p<0.5], p * 100))
+            # print('{} : y={} | y_pred={} (F: {:.2f} %)'.format(len(predictions), gender, {True: 'm', False: 'f'}[p<0.5], p * 100))
+            bar.update(i + 1)
+
     predictions = np.array(predictions)
     print('Accuracy : {:.2f} %'.format(len(predictions[predictions]) / len(predictions) * 100))
     
