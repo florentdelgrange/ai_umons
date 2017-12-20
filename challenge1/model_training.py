970ed2f8ba7a25d3959f9a08d751ca22309165af
radioGiorgio <florent.delgrange@gmail.com>
2017-12-20 14:12:10 +0100

    memory optimization + layer covering

--- challenge1/model_training.py
+++ challenge1/model_training.py
@@ -13,10 +13,10 @@ from keras.models import model_from_json
 from keras.callbacks import ModelCheckpoint
 
 EPOCHS = 42
-BATCH_SIZE = 20
+BATCH_SIZE = 64
 STEPS_PER_EPOCH = 9146 // BATCH_SIZE
 gender_dict = {'m': 0, 'f' : 1}
-CUSTOM_SAVE_PATH = '/home/florent/Dropbox/Info/ai_umons/challenge1'
+CUSTOM_SAVE_PATH = '.'
 
 def save(model, path='.', model_name='xception_gender'):
     # serialize model to JSON
@@ -30,6 +30,7 @@ def save(model, path='.', model_name='xception_gender'):
         save(model, path='.', model_name=model_name)
 
 def preprocess_input(x):
+    x = x.astype(np.float64)
     x /= 255.
     x -= 0.5
     x *= 2.
@@ -38,9 +39,6 @@ def preprocess_input(x):
 def generate_dataset(path='sorted_faces/train', mode='train', rotations=False):
     datagen = ImageDataGenerator(
             rotation_range=30,
-            width_shift_range=0.4,
-            height_shift_range=0.2,
-            shear_range=0.4,
             zoom_range=0.4,
             horizontal_flip=True,
             fill_mode='nearest')
@@ -48,13 +46,13 @@ def generate_dataset(path='sorted_faces/train', mode='train', rotations=False):
     while 1:
         with open('{}/{}_info.txt'.format(path, mode), 'r') as info:
             batch_step = 0
-            X = np.empty([BATCH_SIZE, 299, 299, 3])
+            # memory optimization
+            X = np.empty([BATCH_SIZE, 299, 299, 3], dtype='uint8')
             Y = np.empty([BATCH_SIZE], dtype='uint8')
             for line in info:
                 img_name, gender, age = line.split(' ; ')
                 img = load_img('{}/all/{}'.format(path, img_name), target_size=(299, 299))
-                x = img_to_array(img)
-                x = preprocess_input(x)
+                x = img_to_array(img).astype('uint8')
                 X[batch_step] = x
                 Y[batch_step] = gender_dict[gender]
                 batch_step += 1
@@ -62,9 +60,9 @@ def generate_dataset(path='sorted_faces/train', mode='train', rotations=False):
                 if batch_step == BATCH_SIZE:
                     if rotations:
                         X = datagen.flow(X, batch_size=BATCH_SIZE).next()
-                    yield (X, Y)
+                    yield (preprocess_input(X), Y)
                     batch_step = 0
-                    X = np.empty([BATCH_SIZE, 299, 299, 3])
+                    X = np.empty([BATCH_SIZE, 299, 299, 3], dtype='uint8')
                     Y = np.empty([BATCH_SIZE], dtype='uint8')
 
 def fine_tuning(weights):
@@ -88,9 +86,9 @@ def fine_tuning(weights):
 
     # we chose to train the top 2 inception blocks, i.e. we will freeze
     # the first 249 layers and unfreeze the rest:
-    for layer in model.layers[:249]:
+    for layer in model.layers[:55]:
        layer.trainable = False
-    for layer in model.layers[249:]:
+    for layer in model.layers[55:]:
        layer.trainable = True
 
     # we need to recompile the model for these modifications to take effect
@@ -119,32 +117,35 @@ def fine_tuning(weights):
     save(model, path=CUSTOM_SAVE_PATH, model_name='fine_tuned_xception_gender')
 
 def main_training(weights=''):
-    json_file = open('{}/models/xception_gender.json'.format(CUSTOM_SAVE_PATH), 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    # load weights into new model
-    model.load_weights("{}/models/xception_gender.h5".format(CUSTOM_SAVE_PATH))
-
-    if weights:
-        model.load_weights(weights)
-        print("checkpoint weights loaded!")
+    print("model initialisation (Xception based) ...")
+    base_model = Xception(include_top=False, input_shape=(299, 299, 3))
 
     # let's visualize layer names and layer indices
-    for i, layer in enumerate(model.layers):
+    for i, layer in enumerate(base_model.layers):
        print(i, layer.name)
 
-    # we chose to train the top 3 add blocks (from add block 9), i.e. we will freeze
-    # the first 95 layers and unfreeze the rest:
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(299, activation='relu')(x)
+    # and a logistic layer ; we have 2 classes
+    predictions = Dense(1, activation='sigmoid')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
     for layer in model.layers[:95]:
        layer.trainable = False
     for layer in model.layers[95:]:
        layer.trainable = True
 
     # compile the model (should be done *after* setting layers to non-trainable)
-    model.compile(optimizer='nadam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    print("\nTop layers fitting phase (with rotations)")
+    print("\nTop layers fitting phase (with datagenerator)")
 
     # Callbacks
     if not os.path.exists('{}/weights'.format(CUSTOM_SAVE_PATH)):
@@ -158,7 +159,7 @@ def main_training(weights=''):
     model.fit_generator(generate_dataset(rotations=True), steps_per_epoch=STEPS_PER_EPOCH,
                         validation_data=generate_dataset(path='sorted_faces/valid',
                                                          mode='valid', rotations=True),
-                        validation_steps=50,
+                        validation_steps=15,
                         epochs=EPOCHS, callbacks=[tensorboard, checkpoint])
 
     # at this point, the top layers are well trained and we can start fine-tuning
