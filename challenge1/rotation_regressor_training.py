ea10381718a87faa587dde32040b5c723c161b1e
radioGiorgio <florent.delgrange@gmail.com>
2017-12-23 21:52:29 +0100

    correction of the neural net

--- challenge1/rotation_regressor_training.py
+++ challenge1/rotation_regressor_training.py
@@ -1,4 +1,4 @@
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, GlobalAveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from time import time
 from keras import backend
@@ -10,6 +10,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
 from keras.models import model_from_json
 from keras.callbacks import ModelCheckpoint
+from keras.applications.vgg19 import VGG19
 import os
 
 
@@ -67,15 +68,15 @@ def rmse(y_true, y_pred):
 if __name__=='__main__':
     np.random.seed(42)
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+    model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1),
                      activation='relu',
                      input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(1000, activation='relu'))
-    model.add(Dense(1, activation='softmax', kernel_initializer='normal'))
+    model.add(Dense(150, activation='relu'))
+    model.add(Dense(1))#, kernel_initializer='normal'))
 
     model.compile(loss='mse', optimizer='adam', metrics=[rmse, 'mean_squared_error'])
     if not os.path.exists('{}/weights'.format(CUSTOM_SAVE_PATH)):
@@ -87,7 +88,7 @@ if __name__=='__main__':
 
     # Fit
     model.fit_generator(generate_dataset(), steps_per_epoch=STEPS_PER_EPOCH,
-                        validation_data=generate_dataset(path='sorted_faces/valid'),
+                        validation_data=generate_dataset(path='sorted_faces/valid', mode='valid'),
                         validation_steps=30,
                         epochs=EPOCHS, callbacks=[tensorboard, checkpoint])
 
